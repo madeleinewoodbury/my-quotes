e5c3a83c64dd33695ba579fb00f49ba3d7d13c14
Madeleine Woodbury <randine25@gmail.com>
2019-01-04 20:41:17 -0500

    get quotes from url

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import { ui } from "./ui";
 import { http } from "./http";
+
+// Load all quotes in db on DOM load
+document.addEventListener("DOMContentLoaded", getQuotes);
+
+// Get Quotes
+function getQuotes() {
+  http
+    .get("http://localhost:3000/quotes")
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
--- src/http.js
+++ src/http.js
@@ -1,3 +1,11 @@
-class HTTP {}
+class HTTP {
+  // Make an HTTP Get Request
+  async get(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  }
+}
 
 export const http = new HTTP();
